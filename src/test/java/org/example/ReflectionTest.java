1b9cda1ef298cffee54571977e46dd8c3277bea8
seo-dong-hyeon <62382430+seo-dong-hyeon@users.noreply.github.com>
2023-01-01 23:25:30 +0900

    Reflection practice
    힙 영역에 로드되어 있는 클래스 타입의 객체를 가져오는 3가지 방법

--- src/test/java/org/example/ReflectionTest.java
+++ src/test/java/org/example/ReflectionTest.java
@@ -15,6 +15,8 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
 
@@ -39,5 +41,27 @@ public class ReflectionTest {
         logger.debug("User all declared fields: [{}]", Arrays.stream(clazz.getDeclaredFields()).collect(Collectors.toList()));
         logger.debug("User all declared constructor: [{}]", Arrays.stream(clazz.getDeclaredConstructors()).collect(Collectors.toList()));
         logger.debug("User all declared method: [{}]", Arrays.stream(clazz.getDeclaredMethods()).collect(Collectors.toList()));
+    }
+
+    @Test
+    void load() throws ClassNotFoundException {
+        // 힙 영역에 로드되어 있는 클래스 타입의 객체를 가져오는 방법 1
+        Class<User> clazz = User.class;
+
+        // 힙 영역에 로드되어 있는 클래스 타입의 객체를 가져오는 방법 2
+        // 객체 생성 -> getClass()
+        User user = new User("dhseo", "ㅅㄷㅎ");
+        Class<? extends User> clazz2 = user.getClass();
+
+        // 힙 영역에 로드되어 있는 클래스 타입의 객체를 가져오는 방법 3
+        Class<?> clazz3 = Class.forName("org.example.model.User");
+
+        logger.debug("clazz : [{}],", clazz);
+        logger.debug("clazz2 : [{}],", clazz2);
+        logger.debug("clazz3 : [{}],", clazz3);
+
+        assertThat(clazz == clazz2).isTrue();
+        assertThat(clazz2 == clazz3).isTrue();
+        assertThat(clazz3 == clazz).isTrue();
     }
 }
