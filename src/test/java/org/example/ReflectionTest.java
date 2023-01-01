cd0b171fa4029d52c3f18eafd92fe6a293958a26
seo-dong-hyeon <62382430+seo-dong-hyeon@users.noreply.github.com>
2023-01-01 23:17:56 +0900

    Reflection practice
    class의 정보(이름, 필드, 생성자, 함수) 가져오기

--- src/test/java/org/example/ReflectionTest.java
+++ src/test/java/org/example/ReflectionTest.java
@@ -2,15 +2,18 @@ package org.example;
 
 import org.example.annotaion.Controller;
 import org.example.annotaion.Service;
+import org.example.model.User;
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.annotation.Annotation;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
@@ -27,5 +30,14 @@ public class ReflectionTest {
     void controllerScan(){
         Set<Class<?>> beans = getTypesAnnotatedWith(List.of(Controller.class, Service.class));
         logger.debug("beans : [{}]", beans);
+    }
+
+    @Test
+    void showClass(){
+        Class<User> clazz = User.class;
+        logger.debug(clazz.getName());
+        logger.debug("User all declared fields: [{}]", Arrays.stream(clazz.getDeclaredFields()).collect(Collectors.toList()));
+        logger.debug("User all declared constructor: [{}]", Arrays.stream(clazz.getDeclaredConstructors()).collect(Collectors.toList()));
+        logger.debug("User all declared method: [{}]", Arrays.stream(clazz.getDeclaredMethods()).collect(Collectors.toList()));
     }
 }
